a17fef44eae93e02c9f62db594d8e11f58e62d78
Gologa Virobo <38456916+Golobro@users.noreply.github.com>
2020-02-26 21:46:55 +1100

    Update app.js

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,132 +1,125 @@
 /* ====== Written By Gologa Virobo ====== */
 
-const url = "https://jsonplaceholder.typicode.com/todos";
-const li = qa("li");
-const menu = qs("ul");
-const form = qs('form');
+const url = "https://jsonplaceholder.typicode.com/todos"
+const li = qa("li")
+const menu = qs("ul")
+const form = qs('form')
 
 // Data source
-const getData = server => {
-  fetch(server)
-  .then(res => res.json())
-  .then(data => renderData(data))
-  .catch(err => console.warn(err));
-};    
+const getData = async server => {
+  let res = await fetch(server)
+  let data = await res.json()
+  renderData(data)
+}    
 
-getData(url);
+getData(url)
 
 // Render API Data
-const renderData = data => {  
+const renderData = data => {
   li.forEach((idx, i) => {
-    let { title } = data[i];
-    renderUI(idx, title);
-  });
-};
+    let { title } = data[i]        
+    renderList(idx, title)
+  })
+}
 
-// Render API Data to DOM
-const renderUI = (el, param) => {
-  let iconRender = '<i class="fa fa-trash" onclick="deleteList(this)"></i>';
-  el.innerHTML = `<span class="edit" title="click to edit text" onclick="editNewElem(this)">${param}</span>${iconRender}`;  
-};
+const renderList = (el, text) =>{
+    el.children[0].textContent = text
+    el.innerHTML = el.children[0].outerHTML + el.children[1].outerHTML
+}
 
 // Get form data
-form.onsubmit = e => {
-  e.preventDefault();
-  getTodos();
-}  
+form.addEventListener('submit', getTodos, false)  
+function getTodos(e){
+  e.preventDefault()
+  let formData = new FormData(form)
+  let todo = formData.get('todo-text')
+  let liTag = crEl('li')
 
-const getTodos = () => {
-  let todo = qs('.form-text').value;
-  let liTag = crEl('li');
-  qs('.form-text').value = '';
   // check if input text is empty
-  if (todo.length === 0) {
-    alert("Don't leave empty!");
-    return;
+  if (!todo) {
+    alert("Don't leave empty!")
+    return
   }  
-  loadIcons(liTag);
-  loadSpanTag(liTag, todo);
-  menu.appendChild(liTag);
+
+  // Create UI
+  loadSpanTag(liTag, todo)
+  loadIcons(liTag)
+  menu.appendChild(liTag)
+
+  this.reset()
 }  
 
 // load icons to the dom
 const loadIcons = el => {
   // create El
-  const icon = crEl("i");
+  const icon = crEl("i", "fa fa-trash")
   // set Attr
-  icon.setAttribute("class", "fa fa-trash");
-  icon.setAttribute("onclick", "deleteList(this)");
+  icon.setAttribute("onclick", "deleteList(this)")
   // Append To Li
-  el.appendChild(icon);
-};
+  el.appendChild(icon)
+}
 
 // delete li tags
 const deleteList = el => {
-  el.parentElement.remove();
-};
+  el.parentElement.remove()
+}
 
 // load span tags to the dom
 const loadSpanTag = (el, val) => {
   // create El
-  const span = crEl('span');
-  // Create Txtnode
-  const spanTxt = crTxt(val);
+  const span = crEl('span', 'edit')
   // Set Attr
-  span.setAttribute('class', 'edit');
-  span.setAttribute('title', 'click to edit text');
-  span.setAttribute('onclick', 'editNewElem(this)');
-  // Append Txtnode to span tag
-  span.appendChild(spanTxt);
+  span.textContent = val
+  span.setAttribute('title', 'click to edit text')
+  span.setAttribute('onclick', 'editNewElem(this)')
   // Append span tag to Li
-  el.appendChild(span);
+  el.appendChild(span)
 }
 
 // edit list tags
 const editNewElem = list => {
   // Create El
-  const input = crEl("input");
+  const input = crEl("input", "edit-text")
   // Set Attr
-  input.className = "edit-text";
-  input.placeholder = "Type text and HIT Enter";
-  input.title = 'Type and Hit Enter to make changes to the text';
+  input.placeholder = "Type text and HIT Enter"
+  input.title = 'Type and Hit Enter to make changes to the text'
 
   // Set Vars for DOM manipulation
-  let listNodeName = list.nodeName;
-  let text = list.innerText;  
-  let parentEl = list.parentElement;
+  let listNodeName = list.nodeName
+  let text = list.innerText  
+  let parentEl = list.parentElement
   // add input to li tag
   if (listNodeName === 'SPAN') {
-    input.value = text;
-    renderUI(parentEl, text);
-    parentEl.appendChild(input).focus();    
+    input.value = text
+    renderList(parentEl, text)
+    parentEl.appendChild(input).focus()    
   } 
+
   // remove input when element is blurred
-  input.addEventListener('blur', function(){this.remove();});
+  input.addEventListener('blur', function(){this.remove()})
+
   // change current text on keypress
   input.onkeypress = function(e) {
-    const defaultTxt = e.target.parentElement.textContent;
-    const newTxt = e.target.value;
+    const defaultTxt = e.target.parentElement.textContent
+    const newTxt = e.target.value
     // change text when enter key is pressed
     if (e.key === "Enter") {
       // overwrite default text with new one
-      renderUI(parentEl, newTxt);
-      this.remove();
+      renderList(parentEl, newTxt)
+      this.remove()
       if (newTxt === "") {
         // do not overwrite text if input blank
-        renderUI(parentEl, defaultTxt);
+        renderList(parentEl, defaultTxt)
       } else if (this.value) {
         // clear input field
-        this.value = "";
+        this.value = ""
       }
     }
-  };
-};
+  }
+}
 
 // loop through li tags and apply created elems
 li.forEach(el => {
-  loadSpanTag(el);
-  loadIcons(el);
-});
-
-
-
+  loadSpanTag(el)
+  loadIcons(el)
+})
